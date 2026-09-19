4cc7d26b9aaf1951e79f081f7da580ad9fff2fd4
Athanasios Kourtzis <athkou@hotmail.com>
2026-09-19 08:06:13 +0200

    Added Book extension for JUnit Tests

---
+++ src/test/java/gr/kourtzis/extension/BookResolver.java
@@ -0,0 +1,21 @@
+package gr.kourtzis.extension;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+import gr.kourtzis.library.Book;
+
+public class BookResolver implements ParameterResolver {
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+            throws ParameterResolutionException {
+        return parameterContext.getParameter().getType() == Book.class;
+    }
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+            throws ParameterResolutionException {
+        return new Book();
+    }
+}
