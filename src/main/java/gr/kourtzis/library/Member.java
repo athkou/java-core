7ab09d3210867c3e80b9146ae2afc55c7d88e847
Athanasios Kourtzis <athkou@hotmail.com>
2026-09-26 08:40:21 +0200

    Updated Member class to extend from LibraryUser

--- src/main/java/gr/kourtzis/library/Member.java
+++ src/main/java/gr/kourtzis/library/Member.java
@@ -1,17 +1,15 @@
 package gr.kourtzis.library;
 
-public class Member {
+import lombok.Getter;
+
+@Getter 
+public class Member extends LibraryUser {
     private static int memberCounter;
-    private long id;
-    private String name;
+    
     private int borrowedBooks;
 
     public Member(long id, final String name) {
-        checkId(id);
-        checkName(name);
-        
-        this.id = id;
-        this.name = name;
+        super(id, name);
 
         ++memberCounter;
     }
@@ -30,33 +28,5 @@ public class Member {
 
     public int getBorrowedBooks() {
         return borrowedBooks;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    private void checkId(long id) {
-        if(id <= 0) {
-            throw new IllegalArgumentException("id can't be a negative number!");
-        }
-    }
-
-    private void checkName(final String name) {
-        if(name == null || name.isBlank()) {
-            throw new IllegalArgumentException("name can't be null or blank!");
-        }
     }
 }
